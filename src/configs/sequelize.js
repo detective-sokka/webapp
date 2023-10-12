06ad11bd797ccc2b3a9b03219ae91a5a68a7b2a8
Sai Dutt <113736078+detectivesokka@users.noreply.github.com>
2023-10-12 13:23:11 -0400

    A4: Env variable changes (#32)

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config(); // Load .env file
 
-const userName = process.env.DB_USERNAME || "root";
-const password = process.env.DB_PASSWORD || "root";
+const userName = process.env.DB_USERNAME || 'root';
+const password = process.env.DB_PASSWORD || 'root';
+const ipAddress = process.env.DB_IPADDRESS || '127.0.0.1';
+const dialect = process.env.DB_DIALECT || 'mysql';
+const dbName = process.env.DB_NAME || 'saiDB';
 
-
-const sequelize = new Sequelize("saiDB", userName, password, {
-    host: "127.0.0.1",
-    dialect: "mysql",
+const sequelize = new Sequelize({
+    host: ipAddress,
+    dialect: dialect,
+    database: dbName, 
+    username: userName, 
+    password: password,
 });
 
 
