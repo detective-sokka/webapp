4396b285656bc3c501a5033d0d6038a7ed47d6cc
Sai Dutt <113736078+detectivesokka@users.noreply.github.com>
2023-10-16 19:19:26 -0400

    A3: Removing unnecessary error catch blocks (#34)
    Co-authored-by: detective-sokka <saidutt1997@gmail.com>

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -257,55 +257,60 @@ router.put("/v1/assignments/:id", jsonParser, async (req, res) => {
     return;
   }
 
-  decodeBase64(req.get("Authorization")).then(async (decodeString) => {
-    var user = null;
+  decodeBase64(req.get("Authorization"))
+    .then(async (decodeString) => {
 
-    user = {
-      email: decodeString.split(":")[0],
-      password: decodeString.split(":")[1],
-    };
+      var user = null;
 
-    const account = await Account.findOne({
-      where: { email: user.email },
-    }).catch((err) => {
-      console.log(err);
-      res.send(401).json();
-      return;
-    });
+      user = {
+        email: decodeString.split(":")[0],
+        password: decodeString.split(":")[1],
+      };
 
-    if (!account || !(await account.validPassword(user.password))) {
-      res.status(401).json();
-      return;
-    }
-
-    if (JSON.stringify(req.body) === "{}") {
-      console.log("Empty body");
-      res.status(400).json();
-      return;
-    }
-
-    await Assignment.update(req.body, {
-      where: { id: assignmentId },
-    })
-      .then((result) => {
-        if (result[0] === 1) {
-          // The update was successful
-          res.sendStatus(204);
-        } else {
-          // No rows were updated (assignmentId might not exist)
-          res
-            .status(400)
-            .json();
-        }
-      })
-      .catch((error) => {
-        console.error("Error updating assignment:", error);
-        res.status(400).json();
+      const account = await Account.findOne({
+        where: { email: user.email },
       });
-  }).catch((err) => {
 
-    res.json().status(401);
-  });
+      if (!account || !(await account.validPassword(user.password))) {
+
+        res.status(401).json();
+        return;
+      }
+
+      if (JSON.stringify(req.body) === "{}") {
+
+        throw new Error("Empty body");
+      }
+
+      const assignment = await Assignment.findOne({
+        where: { id: assignmentId },
+      });
+
+      if (account.id !== assignment.userId) {
+
+        res.status(401).json();
+        return;
+      }
+
+      const result = await Assignment.update(req.body, {
+
+        where: { id: assignmentId },
+
+      });
+      
+      if (result[0] === 1) {
+        // The update was successful
+        res.status(204).json();
+
+      } else {
+        // No rows were updated (assignmentId might not exist)
+        throw new Error("assignment doesn't exist");
+      }
+    })
+    .catch(() => {
+
+      res.json().status(400);
+    });
 });
 
 // ************* Default healthz apis *************
@@ -316,22 +321,17 @@ router.get("/healthz", (req, res) => {
   }
 
   if (req.headers["content-length"] !== undefined) {
-    console.log("req.body", req.headers["content-length"]);
     res.status(400).json();
     return;
   }
 
-  console.log(req.query);
-
   sequelize
     .authenticate()
     .then(() => {
-      console.log("Connection has been established successfully.");
       res.status(200).json();
       return;
     })
     .catch((error) => {
-      console.error("Unable to connect to the database: ", error);
       res.status(503).json();
     });
 });
